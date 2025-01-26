3a8911b2f6f8c70475e9171082269b125ca25c47
iambibek-1 <iambibek100@gmail.com>
2025-01-26 13:52:07 +0545

    movie model migrated

--- backend/src/migrations/20250123081633-create-movie.js
+++ backend/src/migrations/20250123081633-create-movie.js
@@ -15,10 +15,16 @@ module.exports = {
       },
       imdbScore: {
         type: Sequelize.DECIMAL(11, 10),
+        defaultValue: 0.0,
+        allowNull: false,
       },
       directorId: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        references:{
+          model:'directors',
+          key: 'id',
+        }
       },
       actors: {
         type: Sequelize.JSON,
@@ -26,21 +32,31 @@ module.exports = {
       genreId: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        references: {
+          model:'genres',
+          key: 'id',
+        }
       },
       description: {
         type: Sequelize.TEXT,
       },
-      thumbnail: {
+      thumbnailUrl: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       embedVideoUrl: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       avgRatings: {
         type: Sequelize.DECIMAL(11, 10),
+        defaultValue: 0.0,
+        allowNull: false,
       },
       totalRatings: {
         type: Sequelize.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
       },
       duration: {
         type: Sequelize.STRING,
@@ -50,34 +66,34 @@ module.exports = {
       },
     });
     // Add foreign key for directorId
-    await queryInterface.addConstraint("Movies", {
-      fields: ["directorId"], // Column name in Movies table
-      type: "foreign key",
-      name: "fk_movies_directorId", // Custom name for the constraint
-      references: {
-        table: "directors", // Target table
-        field: "id", // Target column
-      },
-      onDelete: "CASCADE", // Action when the referenced row is deleted
-      onUpdate: "CASCADE", // Action when the referenced row is updated
-    });
+  //   await queryInterface.addConstraint("Movies", {
+  //     fields: ["directorId"], // Column name in Movies table
+  //     type: "foreign key",
+  //     name: "fk_movies_directorId", // Custom name for the constraint
+  //     references: {
+  //       table: "directors", // Target table
+  //       field: "id", // Target column
+  //     },
+  //     onDelete: "CASCADE", // Action when the referenced row is deleted
+  //     onUpdate: "CASCADE", // Action when the referenced row is updated
+  //   });
 
-    // Add foreign key for genreId
-    await queryInterface.addConstraint("Movies", {
-      fields: ["genreId"], // Column name in Movies table
-      type: "foreign key",
-      name: "fk_movies_genreId", // Custom name for the constraint
-      references: {
-        table: "genres", // Target table
-        field: "id", // Target column
-      },
-      onDelete: "CASCADE", // Action when the referenced row is deleted
-      onUpdate: "CASCADE", // Action when the referenced row is updated
-    });
+  //   // Add foreign key for genreId
+  //   await queryInterface.addConstraint("Movies", {
+  //     fields: ["genreId"], // Column name in Movies table
+  //     type: "foreign key",
+  //     name: "fk_movies_genreId", // Custom name for the constraint
+  //     references: {
+  //       table: "genres", // Target table
+  //       field: "id", // Target column
+  //     },
+  //     onDelete: "CASCADE", // Action when the referenced row is deleted
+  //     onUpdate: "CASCADE", // Action when the referenced row is updated
+    // });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeConstraint("Movies", "fk_movies_directorId");
-    await queryInterface.removeConstraint("Movies", "fk_movies_genreId");
+    // await queryInterface.removeConstraint("Movies", "fk_movies_directorId");
+    // await queryInterface.removeConstraint("Movies", "fk_movies_genreId");
     await queryInterface.dropTable("Movies");
   },
 };
