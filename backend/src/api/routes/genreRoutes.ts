14e3bc99988a8b7e45cda76a3d85a91ac241c9c4
iambibek-1 <iambibek100@gmail.com>
2025-01-27 13:22:18 +0545

    validator added while create a genre

--- backend/src/api/routes/genreRoutes.ts
+++ backend/src/api/routes/genreRoutes.ts
@@ -1,11 +1,14 @@
 import {Router} from "express";
 import { genreController } from "../controllers/genreController";
-import { exceptionHandler } from "../../middlewares";
+import { exceptionHandler, Validator} from "../../middlewares";
+import { createGenre } from "../../validators";
 
 const genreRoutes = Router();
 
 genreRoutes.get('/',exceptionHandler(genreController.getGenre));
-genreRoutes.post('/add',exceptionHandler(genreController.postGenre));
+genreRoutes.post('/add',
+    exceptionHandler(Validator.check(createGenre)),
+    exceptionHandler(genreController.postGenre));
 genreRoutes.patch('/:id',exceptionHandler(genreController.update));
 genreRoutes.delete('/:id',exceptionHandler(genreController.delete));
 
